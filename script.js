c2c1b4a717df79a79f502714f581578464004bbc
Tinatin <omurovatinatin@gmail.com>
2022-03-21 23:28:38 +0600

    refactor: add files

--- script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector(".header__nav-toggler")
-const navList = document.querySelector(".nav__list")
-
-btn.addEventListener("click", (e)=>{
-    e.preventDefault();
-    navList.classList.toggle("hide")
-})
